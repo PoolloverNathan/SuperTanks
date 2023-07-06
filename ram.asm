cf9d8431ede3a02bf2d7436756cadcebab180c41
PoolloverNathan <nathan.kulzer@protonmail.com>
2023-07-05 20:21:07 -0400

    Add wrappers for DMA and more RAM slots

--- ram.asm
+++ ram.asm
@@ -4,14 +4,28 @@ includefrom "supertanks.asm"
 org !RAMSTART
 struct RAM !RAMSTART
     .frame: skip 2
+    .gamemode: skip 1
     .nmi_ran: skip 1
     .oamx: skip $7F
     .oamy: skip $7F
     .oamt: skip $7F
     .oama: skip $7F
     .oamh: skip $7F
+    .tilebuf: skip 32*32*16
+    .players: skip 8
     .end
 endstruct
 !RAMEND = RAM.end
 
 !RAMSIZE = !RAMEND-!RAMSTART
+
+DMAFLAGS = $420B
+struct DMA
+    .method8: skip 1
+    .baddr8: skip 1
+    .aaddr16 .aaddrL: skip 1
+    .aaddrH: skip 1
+    .abank8: skip 1
+    .size16 .sizeL: skip 1
+    .sizeH: skip 1
+endstruct align 16
