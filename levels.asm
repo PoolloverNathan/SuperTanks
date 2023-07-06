fe82128fad5ba6cf748d39fb956574111f87a2e8
PoolloverNathan <nathan.kulzer@protonmail.com>
2023-07-06 14:05:16 -0400

    Remember to add levels.asm

---
+++ levels.asm
@@ -0,0 +1,12 @@
+includefrom "supertanks.asm"
+
+namespace NSLVL
+    SQR:
+        .F incbin "levels/square/fg.map.rev"
+        .B incbin "levels/square/bg.map.rev"
+        .E
+    E:
+namespace off
+
+L3M:
+    incbin "menu.map.rev"
