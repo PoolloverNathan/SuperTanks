f017700da0eb3bbd2c2d15b43c7e0761d10634bc
PoolloverNathan <nathan.kulzer@protonmail.com>
2023-07-07 10:55:29 -0400

    more level stuff

--- levels.asm
+++ levels.asm
@@ -5,8 +5,22 @@ namespace NSLVL
         .F incbin "levels/square/fg.map.rev"
         .B incbin "levels/square/bg.map.rev"
         .E
+    CAU:
+        .F incbin "levels/caution/fg.map.rev"
+        .B incbin "levels/caution/bg.map.rev"
+        .E
     E:
 namespace off
 
 L3M:
     incbin "menu.map.rev"
+
+L3P:
+    incbin "game3.map.rev"
+
+LLL:
+    pushtable
+    table "text.tbl"
+    db " >0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ*"
+    pulltable
+    .E
